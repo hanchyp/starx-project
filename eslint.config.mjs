f5e12a2b5fc43d606879ee498bdf8874ffac780e
”Farhan” <farhancahyapermana@gmail.com>
2025-07-31 22:07:09 +0700

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,18 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "node_modules/**",
+      "lib/openzeppelin-contracts/**",
+    ],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-interface": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
